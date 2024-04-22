f7ae5543df9b3a58624a9db3bcf06031ffb17811
amey <ameynimbalkar83@gmai.com>
2024-04-22 14:58:08 +0530

    commit frontend

---
+++ frontend/src/components/images.js
@@ -0,0 +1,17 @@
+
+import chicken from "./chickens.jpg"
+import veg from "./vegtabe.jpg"
+import profile from "./image/profile.jpg"
+import cart from "./image/cart.png"
+import fruitplate from "./image/fruit_plate-removebg-preview.png"
+const images=
+{
+    chicke:chicken,
+    vegetable:veg,
+    profile:profile,
+    cart:cart,
+    fruitplate:fruitplate
+
+
+};
+export default images;
---
+++ frontend/src/components/pages/privacy policy.js
@@ -0,0 +1,13 @@
+import React  from "react";
+import "./contactus.css"
+import {Link} from "react-router-dom"
+import { useEffect } from "react";
+import { UserChangeContext } from "./Usercontext";
+
+const PrivacyPolicy = ()=>
+{
+    return(
+        
+    )
+}
+export default Contact;
